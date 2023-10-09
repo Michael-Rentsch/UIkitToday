3f32b17f7f21ff9a546bb01f3425a5bb915ecdae
Michael Rentsch <64227211+Michael-Rentsch@users.noreply.github.com>
2023-10-09 22:31:15 +0200

    fixed issue where simulator would crash by replacing main storyboard with the downloaded projects main

---
+++ Today/ListViewController/ReminderListViewController+DataSource.swift
@@ -0,0 +1,8 @@
+//
+//  ReminderListViewController+DataSource.swift
+//  Today
+//
+//  Created by Michael Rentsch on 2023/10/09.
+//
+
+import Foundation
---
+++ Today/Models/Date+Today.swift
@@ -0,0 +1,8 @@
+//
+//  Date+Today.swift
+//  Today
+//
+//  Created by Michael Rentsch on 2023/10/09.
+//
+
+import Foundation
